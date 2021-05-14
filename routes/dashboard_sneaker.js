2ccd15be7a75b490dc9c7ced151f5e9f72c711e5
Aurelie75FR <aurelielo.vi@gmail.com>
2021-05-14 12:14:19 +0200

    correction on views

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,4 +1,9 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
 
+//get all sneakers in the <table>
+//edit one sneaker
+//add one sneaker
+//delete one sneaker
+
 module.exports = router;
