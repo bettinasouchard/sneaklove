4042a3ce2917f05adbd1fb6e12f2bd5851bcc5bb
Aurelie75FR <aurelielo.vi@gmail.com>
2021-05-14 13:41:10 +0200

    add first crud on dashboard_sneaker

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,9 +1,19 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
+const SneakerModel = require("../models/Sneaker");
 
-//get all sneakers in the <table>
-//edit one sneaker
-//add one sneaker
-//delete one sneaker
+//GET all sneakers in the <table>
+router.get("/products_manage", (req, res, next) => {
+  SneakerModel.find()
+    .then((result) => res.render("products_manage.hbs", { sneakers: result }))
+    .catch(next);
+});
+
+// GET edit one sneaker
+
+//POST edit
+//GET add one sneaker
+//POST add
+//GET delete one sneaker
 
 module.exports = router;
